dae5cff00331375583f4ba58cf96a972e2cd7073
Amanda de Almeida <108704270+amandadealmeida@users.noreply.github.com>
2022-11-15 19:56:55 -0300

    Reload quando caracteres === 0

--- src/Javascript/controladores_senha/validar_senha.js
+++ src/Javascript/controladores_senha/validar_senha.js
@@ -15,9 +15,10 @@ function analisarsenha() {
     let forca_senha = 0
     const tamanho = senha.length
 
-   if (tamanho === 0) {
+    if (tamanho === 0) {
         document.location.reload(true)
-    }else if (tamanho >= 4) {
+    }
+    if (tamanho >= 4) {
         if (tamanho <= 7) {
             forca_senha += 10
             console.log("senha curta")
